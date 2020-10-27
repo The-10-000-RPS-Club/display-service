fb93eed62299cabdec3da5b411a3f08ac7fbbeb6
Logan Smith <lsmith2895@yahoo.com>
2020-10-26 18:48:57 -0700

    set up mysql sever pool to access mysql ec2 serverd database

--- database/mysqlserver.js
+++ database/mysqlserver.js
@@ -4,8 +4,9 @@ const mysql = require('mysql');
 // const app = require('../server/index.js');
 
 // const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
+//   host: 'ec2-35-160-57-72.us-west-2.compute.amazonaws.com',
+//   user: 'smith',
+//   password: '*H4CKR34CT0R*',
 //   database: 'display',
 // });
 
@@ -19,17 +20,22 @@ const mysql = require('mysql');
 // module.exports = connection;
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: 'ec2-35-160-57-72.us-west-2.compute.amazonaws.com',
+  user: 'smith',
+  password: 'H4CKR34CT0R!',
   database: 'display',
   connectionLimit: 20,
 });
 
 pool.getConnection((err, connection) => {
   if (err) {
-    console.log( 'error in pool connection: ', err);
+    console.log('error in pool connection: ', err);
+  } else if (connection) {
+    console.log('you have connected to POOL!');
   }
 });
 
 module.exports = pool;
+
+// user: 'smith',
+// password: 'H4CKR34CT0R!',
